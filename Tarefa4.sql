f288bf5dfa413e687d54e837c821339aa695194d
Glayce-stefanny <glaycestefannymartins@gmail.com>
2024-11-13 20:44:17 -0300

    Create Tarefa4.sql

---
+++ Tarefa4.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (id, name, salary, department_id) VALUES (1, 'Alice Silva', 4500.00, 1);
+INSERT INTO employees (id, name, salary, department_id) VALUES (2, 'Bruno Santos', 5500.00, 2);
+INSERT INTO employees (id, name, salary, department_id) VALUES (3, 'Carla Oliveira', 6000.00, 3);
+INSERT INTO employees (id, name, salary, department_id) VALUES (4, 'Diego Costa', 4700.00, 1);
+INSERT INTO employees (id, name, salary, department_id) VALUES (5, 'Fernanda Lima', 5200.00, 2);
