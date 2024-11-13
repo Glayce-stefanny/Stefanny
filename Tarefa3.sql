4ce0c7f500035f1c5f8ecdc90081605fad904101
Glayce-stefanny <glaycestefannymartins@gmail.com>
2024-11-13 20:43:42 -0300

    Create Tarefa3.sql

---
+++ Tarefa3.sql
@@ -0,0 +1,3 @@
+INSERT INTO departments (id, name, location) VALUES (1, 'Recursos Humanos', 'São Paulo');
+INSERT INTO departments (id, name, location) VALUES (2, 'Tecnologia da Informação', 'Rio de Janeiro');
+INSERT INTO departments (id, name, location) VALUES (3, 'Marketing', 'Belo Horizonte');
